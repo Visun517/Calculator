461a0ff5be99b7105d822ef510077042bfc5b4c0
Visun517 <visunpraboda999@gmail.com>
2024-12-04 10:15:29 +0530

    calculation

--- src/main/java/com/assignment/calculator/CalculatorViewController.java
+++ src/main/java/com/assignment/calculator/CalculatorViewController.java
@@ -80,96 +80,153 @@ public class CalculatorViewController implements Initializable {
 
     @FXML
     void btn0OnAction(ActionEvent event) {
+        setTetxtFeild("0");
 
     }
 
     @FXML
     void btn1OnAction(ActionEvent event) {
-
+        setTetxtFeild("1");
     }
 
     @FXML
     void btn2OnAction(ActionEvent event) {
-
+        setTetxtFeild("2");
     }
 
     @FXML
     void btn3OnAction(ActionEvent event) {
-
+        setTetxtFeild("3");
     }
 
     @FXML
     void btn4OnAction(ActionEvent event) {
-
+        setTetxtFeild("4");
     }
 
     @FXML
     void btn5OnAction(ActionEvent event) {
-
+        setTetxtFeild("5");
     }
 
     @FXML
     void btn6OnAction(ActionEvent event) {
-
+        setTetxtFeild("6");
     }
 
     @FXML
     void btn7OnAction(ActionEvent event) {
-
+        setTetxtFeild("7");
     }
 
     @FXML
     void btn8OnAction(ActionEvent event) {
-
+        setTetxtFeild("8");
     }
 
     @FXML
     void btn9OnAction(ActionEvent event) {
-
+        setTetxtFeild("9");
     }
 
     @FXML
     void btnDivedOnAction(ActionEvent event) {
-
+        setTetxtFeild("/");
     }
 
     @FXML
     void btnDotOnAction(ActionEvent event) {
-
+        setTetxtFeild(".");
     }
 
     @FXML
     void btnEqualONAction(ActionEvent event) {
+        char[] text = txtType.getText().toCharArray();
+        char [] operators = {'+','-','*','/','%'};
+        int operatorIndex = 0;
+        char operator = 0;
 
+        for (int i = 0; i < text.length; i++) {
+            for (int j = 0; j <operators.length; j++) {
+                if (text[i] == operators[j]){
+                    operatorIndex = i;
+                    operator = operators[j];
+                }
+            }
+        }
+        String textField = txtType.getText();
+        String subString = textField.substring(0,operatorIndex);
+        String otherString = textField.substring(operatorIndex+1);
+        System.out.println(subString);
+        System.out.println(otherString);
+        System.out.println(operator);
+
+        calculation(subString,otherString,operator);
+    }
+
+    private void calculation(String subString, String otherString, char operator) {
+        int subString1 = Integer.parseInt(subString);
+        int otherString1 = Integer.parseInt(otherString);
+
+        System.out.println(subString1+""+operator+""+otherString1);
+
+        if (operator == '+'){
+            int result = subString1+otherString1;
+            System.out.println(result);
+            txtType.setText(String.valueOf(result));
+        }
+        if (operator == '-'){
+            int result = subString1-otherString1;
+            System.out.println(result);
+            txtType.setText(String.valueOf(result));
+        }
+        if (operator == '*'){
+            int result = subString1*otherString1;
+            System.out.println(result);
+            txtType.setText(String.valueOf(result));
+        }
+        if (operator == '/'){
+            int result = subString1/otherString1;
+            System.out.println(result);
+            txtType.setText(String.valueOf(result));
+        }
+        if (operator == '%'){
+            int result = subString1%otherString1;
+            System.out.println(result);
+            txtType.setText(String.valueOf(result));
+        }
     }
 
     @FXML
     void btnMultiplyonAction(ActionEvent event) {
-
+        setTetxtFeild("*");
     }
 
     @FXML
     void btnOneNumberberCelatOnAction(ActionEvent event) {
-
     }
 
     @FXML
     void btnPlusOnAction(ActionEvent event) {
-
+        setTetxtFeild("+");
     }
 
     @FXML
     void btnPrecentageOnAction(ActionEvent event) {
-
+        setTetxtFeild("%");
     }
 
     @FXML
     void btnReducingOnAction(ActionEvent event) {
-
+        setTetxtFeild("-");
     }
 
     @FXML
     void btuClearOnAction(ActionEvent event) {
 
+    }
+    private void setTetxtFeild(String text){
+        String oldText = txtType.getText();
+        txtType.setText(oldText+text);
     }
 }
